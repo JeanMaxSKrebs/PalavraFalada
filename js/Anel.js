f7f27d9e96ac514913e1f41ac5b6964e50979da1
Jean Max S Krebs <jeanmaxskrebs@gmail.com>
2021-11-11 14:24:48 -0300

    teste 3

--- js/Anel.js
+++ js/Anel.js
@@ -13,7 +13,6 @@ class Anel
  preload()
 
  {
-   this.load.image("ceu","assets/FA2.png");
    this.load.image("Bola","assets/FonemaB/Bola2.png");
    this.load.image("Bola2","assets/FonemaB/Bola3.png");
    this.load.image("Abelha","assets/FonemaB/Abelha2.png");
--- js/Bala.js
+++ js/Bala.js
@@ -15,7 +15,6 @@ class Bala
  preload()
 
  {
-   this.load.image("ceu","assets/FA3.png");
    this.load.image("Bola","assets/FonemaB/Bola2.png");
    this.load.image("Bola2","assets/FonemaB/Bola3.png");
    this.load.image("Abelha","assets/FonemaB/Abelha2.png");
